4e43d931e7799bfc2b74e908c2a451d6d66a2796
Cassio Leite <cassioabl@hotmail.com>
2022-11-28 23:41:06 +0000

    add groups sql file

---
+++ add_groups.sql
@@ -0,0 +1,41 @@
+update Teams set [Group] = 1 where name = 'Catar'
+update Teams set [Group] = 1 where name = 'Equador'
+update Teams set [Group] = 1 where name = 'Holanda'
+update Teams set [Group] = 1 where name = 'Senegal'
+
+update Teams set [Group] = 2 where name = 'Estados Unidos'
+update Teams set [Group] = 2 where name = 'Inglaterra'
+update Teams set [Group] = 2 where name = 'Irã'
+update Teams set [Group] = 2 where name = 'País de Gales'
+
+update Teams set [Group] = 3 where name = 'Arábia Saudita'
+update Teams set [Group] = 3 where name = 'Argentina'
+update Teams set [Group] = 3 where name = 'México'
+update Teams set [Group] = 3 where name = 'Polônia'
+
+update Teams set [Group] = 4 where name = 'Dinamarca'
+update Teams set [Group] = 4 where name = 'Austrália'
+update Teams set [Group] = 4 where name = 'França'
+update Teams set [Group] = 4 where name = 'Tunísia'
+
+update Teams set [Group] = 5 where name = 'Alemanha'
+update Teams set [Group] = 5 where name = 'Costa Rica'
+update Teams set [Group] = 5 where name = 'Espanha'
+update Teams set [Group] = 5 where name = 'Japão'
+
+update Teams set [Group] = 6 where name = 'Bélgica'
+update Teams set [Group] = 6 where name = 'Canadá'
+update Teams set [Group] = 6 where name = 'Corácia'
+update Teams set [Group] = 6 where name = 'Marrocos'
+
+
+update Teams set [Group] = 7 where name = 'Brasil'
+update Teams set [Group] = 7 where name = 'Camarões'
+update Teams set [Group] = 7 where name = 'Sérvia'
+update Teams set [Group] = 7 where name = 'Suíça'
+
+update Teams set [Group] = 8 where name = 'Coreia do Sul'
+update Teams set [Group] = 8 where name = 'Gana'
+update Teams set [Group] = 8 where name = 'Portugal'
+update Teams set [Group] = 8 where name = 'Uruguai'
+
